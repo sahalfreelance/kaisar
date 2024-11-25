7a5fd17a8acdff2fb42064e55ffc1a2ea430b451
sahalfreelance <110083545+sahalfreelance@users.noreply.github.com>
2024-11-25 22:23:10 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt install python3
+sudo apt install python3-pip
+pip3 install requests
+pip3 install uuid
+pip3 install loguru
+pip3 install asyncio
+pip3 install fake_useragent
+pip3 install cloudscraper
+clear
+python3 kaisar.py
